c7e31c850e37b22fd20b614c329ecdc1da31d9d0
Izhmael-school <mcd2476033@stu.o-hara.ac.jp>
2025-10-01 14:27:41 +0900

    Dxlib Setup

---
+++ Hacslike/Scr/Definition.h
@@ -0,0 +1,64 @@
+#pragma once
+//	=================================================================
+//		ヘッダーをインクルードする場所
+//	=================================================================
+#include <DxLib.h>
+#include <string>
+
+//	=================================================================
+//		定数を定義する場所
+//	=================================================================
+#define WINDOW_WIDTH			(1200)	//	ウィンドウの横幅
+#define WINDOW_HEIGHT			(800)	//	ウィンドウの縦幅
+
+#define FPS						(60)	//	FPS
+
+//	引数付きマクロ定義 変換マクロ
+#define Deg2Rad(x) ( x * DX_PI_F / 180.0f ) //　デグリー角→ラジアン角
+#define Rad2Deg(x) ( x * 180.0f / DX_PI_F ) //  ラジアン角→デグリー角
+
+// ワールドベクトル
+#define VRight	 (VGet(1.0f,0,0))
+#define VLeft	 (VGet(-1.0f,0,0))
+#define VUp		 (VGet(0,1.0f,0))
+#define VDown	 (VGet(0,-1.0f,0))
+#define VForward (VGet(0,0,1.0f))
+#define VBack    (VGet(0,0,-1.0f))
+
+// ゼロベクトル
+#define VZero   (VGet(0,0,0))
+#define VOne   (VGet(1,1,1))
+
+// DxLibの読み込みエラ-
+#define INVALID (-1)
+
+#define EnemyMax (10)
+
+#define DeckMax (9)
+#define HandMax (5)
+//	=================================================================
+//		定数を定義する場所
+//	=================================================================
+//	色
+const unsigned int red = GetColor(255, 0, 0);
+const unsigned int green = GetColor(0, 255, 0);
+const unsigned int blue = GetColor(0, 0, 255);
+const unsigned int magenta = GetColor(255, 0, 255);
+const unsigned int cyan = GetColor(0, 255, 255);
+const unsigned int yellow = GetColor(255, 255, 0);
+const unsigned int white = GetColor(255, 255, 255);
+const unsigned int gray = GetColor(120, 120, 120);
+const unsigned int black = GetColor(0, 0, 0);
+
+enum class SceneType {
+	Title,
+	Game,
+	Clear,
+	HowTo,
+};
+
+enum class FadeState {
+	FadeIn = -1,
+	FadeOut = 1,
+	FadeEnd,
+};
---
+++ Hacslike/Scr/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <Dxlib.h>
+#include "Definition.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+#pragma region // DxLibの初期化処理　触るべからず
+	// ウィンドウのサイズを変更する
+	SetGraphMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32, FPS);
+	// 起動時のウィンドウのモードの設定
+	ChangeWindowMode(TRUE);	// TRUE : ウィンドウモード FALSE : フルスクリーン
+	// 背景色の設定
+	SetBackgroundColor(196, 196, 196);
+
+	// Dxlibの初期化
+	if (DxLib_Init() == -1)
+		return 0;
+
+	// Effekseerの初期化
+	/*if (Effekseer_Init(8000) == -1) {
+		DxLib_End();
+		return 0;
+	}*/
+
+	// 描画する先を設定する 裏画面に変更する
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	// 図形描画のZバッファの有効化
+	{
+		// Zバッファを使用するかどうか
+		SetUseZBuffer3D(TRUE);	// default : FALSE
+		// Zバッファに書き込みを行うか
+		SetWriteZBuffer3D(TRUE); // default : FALSE
+	}
+
+	// ライティング
+	{
+		// ライトの計算をどうするか
+		SetUseLighting(TRUE); // default : TRUE
+		// 標準ライトを使用するかどうか
+		SetLightEnable(TRUE);	// default : TRUE
+		// グローバル環境光の設定
+		SetGlobalAmbientLight(GetColorF(1, 0, 0, 0));
+		//// 反射光の設定  Diffuse
+		//SetLightDifColor(GetColorF(1, 0, 0, 0));
+		//// 鏡面反射光の設定　Specular
+		//SetLightSpcColor(GetColorF(1, 0, 0.25f, 1));
+		//// 環境光の設定　Ambient
+		//SetLightAmbColor(GetColorF(1, 1, 1, 1));
+	}
+#pragma endregion
+
+
+	// ゲームのメインループ
+	while (true) {
+
+		break;
+	}
+
+	// DxLibの終了
+	DxLib_End();
+
+	return 0;
+}
