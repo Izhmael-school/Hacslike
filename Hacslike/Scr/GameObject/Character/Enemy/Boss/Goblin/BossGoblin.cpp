ca9ecbd179c3b31ea696ee4f9aaa703b73245083
Izhmael-school <mcd2476033@stu.o-hara.ac.jp>
2026-01-20 12:35:31 +0900

    Fix Goblin Boss

--- Hacslike/Scr/GameObject/Character/Enemy/Boss/Goblin/BossGoblin.cpp
+++ Hacslike/Scr/GameObject/Character/Enemy/Boss/Goblin/BossGoblin.cpp
@@ -18,17 +18,17 @@ void BossGoblin::Start() {
 	pCollider = new CapsuleCollider(this, VGet(0, 30, 0), VGet(0, 800, 0), 50);
 
 	// UŒ‚‚Ì“–‚½‚è”»’è
-	pAnimator->GetAnimation("attack01")->SetEvent([this]() { attackColliderList.push_back(new SphereHitBox(this, VZero, 200, 2 / GetFPS())); }, 14);
-	pAnimator->GetAnimation("attack02")->SetEvent([this]() { attackColliderList.push_back(new SphereHitBox(this, VZero, 200, 3 / GetFPS())); }, 12);
-	pAnimator->GetAnimation("attack02")->SetEvent([this]() { attackColliderList.push_back(new SphereHitBox(this, VZero, 200, 2 / GetFPS())); }, 24);
-	pAnimator->GetAnimation("attack03")->SetEvent([this]() { attackColliderList.push_back(new SphereHitBox(this, VZero, 200, 2 / GetFPS())); }, 32);
+	SetAnimEventForAttackCollider("attack01", 14, 2, 150, 150);
+	SetAnimEventForAttackCollider("attack02", 12, 2, 150, 150);
+	SetAnimEventForAttackCollider("attack02", 24, 2, 120, 150);
+	SetAnimEventForAttackCollider("attack03", 33, 2, 200, 150);
 	// UŒ‚’†‚ÌˆÚ“®§Œä
-	pAnimator->GetAnimation("attack01")->SetEvent([this]() { SetAttacking(true); }, 8);
-	pAnimator->GetAnimation("attack01")->SetEvent([this]() { SetAttacking(false); }, pAnimator->GetTotalTime("attack01"));
-	pAnimator->GetAnimation("attack02")->SetEvent([this]() { SetAttacking(true); }, 8);
-	pAnimator->GetAnimation("attack02")->SetEvent([this]() { SetAttacking(false); }, pAnimator->GetTotalTime("attack02"));
-	pAnimator->GetAnimation("attack03")->SetEvent([this]() { SetAttacking(true); }, 8);
-	pAnimator->GetAnimation("attack03")->SetEvent([this]() { SetAttacking(false); }, pAnimator->GetTotalTime("attack03"));
+	SetAnimEvent("attack01", [this]() { SetAttacking(true); }, 0);
+	SetAnimEvent("attack01", [this]() { SetAttacking(false); }, pAnimator->GetTotalTime("attack01"));
+	SetAnimEvent("attack02", [this]() { SetAttacking(true); }, 0);
+	SetAnimEvent("attack02", [this]() { SetAttacking(false); }, pAnimator->GetTotalTime("attack02"));
+	SetAnimEvent("attack03", [this]() { SetAttacking(true); }, 0);
+	SetAnimEvent("attack03", [this]() { SetAttacking(false); }, pAnimator->GetTotalTime("attack03"));
 	// UŒ‚‚ÌŒø‰Ê‰¹
 	pAnimator->GetAnimation("attack01")->SetEvent([this]() {AudioManager::GetInstance().PlayOneShot("SwordSwing"); }, 14);
 	pAnimator->GetAnimation("attack02")->SetEvent([this]() {AudioManager::GetInstance().PlayOneShot("SwordSwing"); }, 12);
