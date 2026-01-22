c1e15915e75ba5268872028c5c1c46d37eb23933
Izhmael-school <mcd2476033@stu.o-hara.ac.jp>
2026-01-22 11:49:51 +0900

    delete scene

--- Hacslike/Scr/Manager/SceneManager.cpp
+++ Hacslike/Scr/Manager/SceneManager.cpp
@@ -20,7 +20,6 @@ SceneManager::~SceneManager() {
 
 void SceneManager::Start() {
 	pSceneList[(int)SceneType::Title] = new TitleScene();
-	pSceneList[(int)SceneType::Sekino] = new SekinoScene();
 	pSceneList[(int)SceneType::Game] = new GameScene();
 	//pSceneList[(int)SceneType::Sekino] = new SekinoScene();
 }
